e6625ce9a6666e7c8d3c9e16bebf3d664f573ecc
Xxpepitogameplays2007xX <caalhernandezdiego@gmail.com>
2026-05-21 10:23:41 -0600

    fix eslint and update next

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
-import nextVitals from 'eslint-config-next/core-web-vitals.js'
-import nextTs from 'eslint-config-next/typescript.js'
+import nextVitals from "eslint-config-next/core-web-vitals.js";
+import nextTs from "eslint-config-next/typescript.js";
 
 export default [
-  ...nextVitals,
-  ...nextTs,
-]
+  nextVitals,
+  nextTs,
+];
